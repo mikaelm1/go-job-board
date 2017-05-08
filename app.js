64b9d64d10c1c495bae6b1f059f70f65bc59a98c
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-05-07 20:30:00 -0700

    Added base views with nav bar and bootstrap

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
+var handlebars = require("express-handlebars").create({defaultLayout: "main"});
+var bodyParser = require("body-parser");
 var express = require('express');
 var app = express();
 
 app.set('port', 8000);
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
+
+app.get('/', function(req, res){
+    res.render('home');
+});
 
 app.get('/health', function(req, res){
     res.type('application/json');
