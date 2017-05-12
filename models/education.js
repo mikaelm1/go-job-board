c58b59e699c8ea626961834310cb323013e04192
Mikael Mukhsikaroyan <mikaelm3981@gmail.com>
2017-05-11 21:15:34 -0700

    Can add education to profile

--- models/education.js
+++ models/education.js
@@ -14,7 +14,39 @@ var pool = mysql.createPool({
   database        : 'job_board'
 });
 
-function User(name, major) {
+function Education(name, major, yearStarted, yearEnded, gpa) {
     this.name = name;
     this.major = major;
+    this.yearStarted = yearStarted;
+    this.yearEnded = yearEnded;
+    this.gpa = gpa;
 }
+
+Education.prototype.create = function(uid, callback){
+    console.log('UID: ' + uid);
+    console.log(this.name, this.major, this.gpa, this.yearStarted, this.yearEnded);
+    pool.query({
+        sql: 'INSERT INTO education (name, major, year_started, year_graduated, gpa, user_id)  VALUES (?, ?, ?, ?, ?, ?)',
+        timeout: 40000,
+        values: [this.name, this.major, this.yearStarted, this.yearEnded, this.gpa, uid]
+    }, function(err, result){
+        if (err) {
+            console.log('Error: ' + err);
+            callback(err, null);
+        } else {
+            // console.log(result);
+            // ed = {
+            //     id: result[0].id,
+            //     name: result[0].name,
+            //     major: result[0].major,
+            //     yearStarted: result[0].year_started,
+            //     yearEnded: result[0].year_graduated,
+            //     gpa: result[0].gpa,
+            //     uid: result[0].uid,
+            // }
+            callback(null, 'success');
+        }
+    });
+}
+
+module.exports = Education;
--- routes/education.js
+++ routes/education.js
@@ -1,22 +1,40 @@
 var express = require("express");
 var router = express.Router();
-var User = require("../models/user");
+var Education = require("../models/education");
 var auth = require("../middleware/auth");
 
 router.get('/', auth.isSeeker, function(req, res){
-    // res.locals.expressFlash = req.flash('danger', 'Hey');
-    // res.locals.flashType = 'danger';
-    console.log('INSIDE education/');
-    res.locals.sessionFlash = {
-        type: 'success',
-        message: 'This is a flash message using custom middleware and express-session.'
-    }
+    // console.log('INSIDE education/');
+    // res.locals.sessionFlash = {
+    //     type: 'success',
+    //     message: 'This is a flash message using custom middleware and express-session.'
+    // }
     res.render('education/new');
 });
 
-router.post('/', auth.isSeeker, function(req, res){
-    req.flash('danger', 'Still not finished');
-    res.render('education/new');
+router.post('/new', auth.isSeeker, function(req, res){
+    var name = req.body.name;
+    var major = req.body.major;
+    var yearStarted = Number(req.body.yearStarted);
+    var yearEnded = Number(req.body.yearEnded);
+    var gpa = Number(req.body.gpa);
+    var education = new Education(name, major, yearStarted, yearEnded, gpa);
+    education.create(req.session.userID, function(err, ed){
+        if (err){
+            req.session.sessionFlash = {
+                type: 'danger',
+                message: 'Error: ' + err,
+            }
+            res.redirect('/education/');
+        } else {
+            req.session.sessionFlash = {
+                type: 'success',
+                message: 'Education added!',
+            }
+            res.redirect('/user/profile');
+        }
+    }); 
+    // res.render('education/new');
 });
 
 
